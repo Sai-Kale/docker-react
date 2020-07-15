2249253cdee9e94ca4a94d8509c069787a8a60b8
Sai-Kale <saikumar.aero@gmail.com>
2020-07-15 15:28:18 -0500

    travisss

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          lets deploy
+          please work
         </p>
         <a
           className="App-link"
