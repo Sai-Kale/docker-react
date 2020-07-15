22e316ec94d2b10f8d6c237aceaf8904c07ee3a0
Sai-Kale <saikumar.aero@gmail.com>
2020-07-15 15:37:36 -0500

    tra

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          please work
+          work out this shit
         </p>
         <a
           className="App-link"
